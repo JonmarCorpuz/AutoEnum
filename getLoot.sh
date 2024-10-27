2e57c64d780f425a98d81132030df66b2bb19d16
Jonmar Corpuz <121370426+JonmarCorpuz@users.noreply.github.com>
2024-10-27 09:22:52 -0400

    Update getLoot.sh

--- getLoot.sh
+++ getLoot.sh
@@ -1,8 +1,15 @@
-# Check if a post scan was done with pScan.sh
+# Check if FTP port is listening
+cat ./Outputs/grep*.txt | grep -q "Host" > ScannedHosts.txt
+
+if [ -s ScannedHosts.txt ]; then
+        # The file is not-empty.
+      
+else
+        # The file is empty.
+fi
 
 # Check if anonymous login is allowed on FTP
-
-if grep -q $TargetAddress 
+if grep -q "Anonymous FTP login allowed";
 then 
    echo "OK"
 #   wget -m --no-passive ftp://anonymous:anonymous@<TARGET_IP>
