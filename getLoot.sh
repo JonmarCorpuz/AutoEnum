8cc37a1f539c07901fc77bb8e938db062553e615
Jonmar Corpuz <121370426+JonmarCorpuz@users.noreply.github.com>
2024-10-27 17:53:00 -0400

    Update getLoot.sh

--- getLoot.sh
+++ getLoot.sh
@@ -1,5 +1,35 @@
 #!/bin/bash
 
+####################################### STATIC VARIABLES ########################################
+
+# Text Color
+WHITE="\033[0m"
+RED="\033[0;31m"
+YELLOW="\033[1;33m"
+GREEN="\033[0;32m"
+
+# Infinte Loop
+ALWAYS_TRUE=true
+
+# Regex
+Integer='^[0-9]+$'
+
+Replace="/"
+
+########################################### OVERVIEW ############################################
+
+echo '''                                                                                              
+                   mm    7MMF                          mm    
+                   MM     MM                            MM    
+ .PYbbmmm .gPPYa mmMMmm   MM         ,pWwWq.   ,pWwWq.mmMMmm  
+:MI  I8  ,M    Yb  MM     MM        6W     Wb 6W     Wb MM    
+ WmmmP   8M""""""  MM     MM      , 8M     M8 8M     M8 MM    
+8M       YM.    ,  MM     MM     ,M YA.   ,A9 YA.   ,A9 MM    
+ YMMMMMb  `Mbmmd   `Mbmo.JMMmmmmMMM  `Ybmd9    `Ybmd9   `Mbmo 
+6      dP                                                     
+Ybmmmdd                                                                                                                                                                                                                                                                   
+'''
+
 #
 cat ./Outputs/grep*.txt | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" >>
 
@@ -17,7 +47,7 @@ if [ -s ScannedHosts.txt ]; then
     done < ScannedHosts.txt
 else
     # The file is empty.
-    echo "NAH"
+    echo "NAH" && exit
 fi
 
 # Check if anonymous login is allowed on FTP
