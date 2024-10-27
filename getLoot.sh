95b5732756716184d52a9a0194ce8b3841795977
Jonmar Corpuz <121370426+JonmarCorpuz@users.noreply.github.com>
2024-10-27 18:05:41 -0400

    Update getLoot.sh

--- getLoot.sh
+++ getLoot.sh
@@ -29,13 +29,14 @@ Ybmmmdd
 '''
 
 #
-cat ./Outputs/grep*.txt | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" >>
+cat ./Outputs/grep*.txt | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" >> ScannedHosts.txt
+
+# Remove network address
 
 if [ -s ScannedHosts.txt ]; then
     # The file is not-empty.
     while read Host;
     do
-        echo $Host
         #
         if ! cat TargetHosts.txt | grep -q $Host;
         then
